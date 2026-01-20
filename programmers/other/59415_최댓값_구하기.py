60f3c0d3cc422b007182b84e44e507c7fbd02a23
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:37:28 +0900

    [프로그래머스] 최댓값 구하기 풀이 제출
    - 문제 ID: 59415
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:37:26
    - 작성자: 박정수

---
+++ programmers/other/59415_최댓값_구하기.py
@@ -0,0 +1,11 @@
+# 최댓값 구하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59415
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:37:26
+
+-- 코드를 입력하세요
+SELECT DATETIME as 시간
+from ANIMAL_INS 
+order by DATETIME desc
+limit 1
