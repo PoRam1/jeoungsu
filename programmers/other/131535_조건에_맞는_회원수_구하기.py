be794f3bf48abaf5efb264d3287fcf0d026ee367
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:36:07 +0900

    [프로그래머스] 조건에 맞는 회원수 구하기 풀이 제출
    - 문제 ID: 131535
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:36:05
    - 작성자: 박정수

--- programmers/other/131535_조건에_맞는_회원수_구하기.py
+++ programmers/other/131535_조건에_맞는_회원수_구하기.py
@@ -2,10 +2,10 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131535
 # 작성자: 박정수
-# 작성일: 2026. 01. 20. 09:34:49
+# 작성일: 2026. 01. 20. 09:36:05
 
 -- 코드를 입력하세요
 SELECT 
-    count(*) as USRS
+    count(*) as USERS
 from USER_INFO 
 where YEAR(JOINED) = 2021 and AGE between 20 and 29
