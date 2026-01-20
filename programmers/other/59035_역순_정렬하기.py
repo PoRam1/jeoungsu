b2c7b207ebff5c925c8a46fbf004d2b2122f9c5d
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:27:23 +0900

    [프로그래머스] 역순 정렬하기 풀이 제출
    - 문제 ID: 59035
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:27:21
    - 작성자: 박정수

---
+++ programmers/other/59035_역순_정렬하기.py
@@ -0,0 +1,12 @@
+# 역순 정렬하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59035
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:27:21
+
+-- 코드를 입력하세요
+SELECT 
+    NAME,
+    DATETIME
+from ANIMAL_INS 
+order by ANIMAL_ID desc
