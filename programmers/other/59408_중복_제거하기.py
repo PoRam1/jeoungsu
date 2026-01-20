4b0935fd6392f347f418aa24a3e3685b8e1b374b
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:27:48 +0900

    [프로그래머스] 중복 제거하기 풀이 제출
    - 문제 ID: 59408
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:27:46
    - 작성자: 박정수

---
+++ programmers/other/59408_중복_제거하기.py
@@ -0,0 +1,9 @@
+# 중복 제거하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59408
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:27:46
+
+-- 코드를 입력하세요
+SELECT count(distinct name)
+from ANIMAL_INS
