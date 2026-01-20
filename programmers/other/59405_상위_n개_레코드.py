73b785c07675eace35fa5579cc8d5ede931ed136
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:28:38 +0900

    [프로그래머스] 상위 n개 레코드 풀이 제출
    - 문제 ID: 59405
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:28:36
    - 작성자: 박정수

---
+++ programmers/other/59405_상위_n개_레코드.py
@@ -0,0 +1,11 @@
+# 상위 n개 레코드
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:28:36
+
+-- 코드를 입력하세요
+select NAME
+from ANIMAL_INS 
+order by DATETIME
+limit 1
