530cf165ac31ade993b830908a6af2476232924e
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:28:16 +0900

    [프로그래머스] 동물의 아이디와 이름 풀이 제출
    - 문제 ID: 59403
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:28:14
    - 작성자: 박정수

---
+++ programmers/other/59403_동물의_아이디와_이름.py
@@ -0,0 +1,11 @@
+# 동물의 아이디와 이름
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:28:14
+
+-- 코드를 입력하세요
+SELECT 
+ANIMAL_ID,
+NAME
+from ANIMAL_INS
