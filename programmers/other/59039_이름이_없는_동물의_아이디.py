a93071f15b506f3228bcb094087fdef22e089d0b
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:34:37 +0900

    [프로그래머스] 이름이 없는 동물의 아이디 풀이 제출
    - 문제 ID: 59039
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:34:34
    - 작성자: 박정수

---
+++ programmers/other/59039_이름이_없는_동물의_아이디.py
@@ -0,0 +1,10 @@
+# 이름이 없는 동물의 아이디
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59039
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:34:34
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID
+from ANIMAL_INS 
+where name is null
