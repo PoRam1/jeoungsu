1545fbb7b98c4e0622082e58644b792e41c5dd1d
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:28:27 +0900

    [프로그래머스] 아픈 동물 찾기 풀이 제출
    - 문제 ID: 59036
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:28:25
    - 작성자: 박정수

---
+++ programmers/other/59036_아픈_동물_찾기.py
@@ -0,0 +1,12 @@
+# 아픈 동물 찾기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59036
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:28:25
+
+-- 코드를 입력하세요
+SELECT 
+    ANIMAL_ID,
+    NAME
+from ANIMAL_INS
+where INTAKE_CONDITION = 'Sick'
