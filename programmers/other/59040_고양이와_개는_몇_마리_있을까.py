69806565b47a075d245c3f02f3c6cd6b0e7a43b4
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:38:16 +0900

    [프로그래머스] 고양이와 개는 몇 마리 있을까 풀이 제출
    - 문제 ID: 59040
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:38:13
    - 작성자: 박정수

---
+++ programmers/other/59040_고양이와_개는_몇_마리_있을까.py
@@ -0,0 +1,13 @@
+# 고양이와 개는 몇 마리 있을까
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59040
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:38:13
+
+-- 코드를 입력하세요
+SELECT 
+    ANIMAL_TYPE,
+    count(*) as count
+from ANIMAL_INS
+group by ANIMAL_TYPE
+order by ANIMAL_TYPE
