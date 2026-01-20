20aedc3d27551205b586926139c767ec1201d872
PoRam1 <yyy3365@gmail.com>
2026-01-20 09:29:19 +0900

    [프로그래머스] 나이 정보가 없는 회원 수 구하기 풀이 제출
    - 문제 ID: 131528
    - 난이도: unknown
    - 제출일: 2026. 01. 20. 09:29:16
    - 작성자: 박정수

---
+++ programmers/other/131528_나이_정보가_없는_회원_수_구하기.py
@@ -0,0 +1,11 @@
+# 나이 정보가 없는 회원 수 구하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131528
+# 작성자: 박정수
+# 작성일: 2026. 01. 20. 09:29:16
+
+-- 코드를 입력하세요
+SELECT
+    Count(USER_ID) as USERS
+from USER_INFO 
+where AGE is null
