b4f37c79b231cf426bd82ccf5eb32bc399dd73b5
PoRam1 <yyy3365@gmail.com>
2026-01-21 09:39:07 +0900

    [프로그래머스] 성분으로 구분한 아이스크림 총 주문량 풀이 제출
    - 문제 ID: 133026
    - 난이도: unknown
    - 제출일: 2026. 01. 21. 09:39:05
    - 작성자: 박정수

---
+++ programmers/other/133026_성분으로_구분한_아이스크림_총_주문량.py
@@ -0,0 +1,17 @@
+# 성분으로 구분한 아이스크림 총 주문량
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133026
+# 작성자: 박정수
+# 작성일: 2026. 01. 21. 09:39:05
+
+SELECT INGREDIENT_TYPE, SUM(TOTAL_ORDER) TOTAL_ORDER
+
+FROM FIRST_HALF F
+
+JOIN ICECREAM_INFO I
+
+ON F.FLAVOR = I.FLAVOR
+
+GROUP BY INGREDIENT_TYPE
+
+ORDER BY TOTAL_ORDER ASC;
