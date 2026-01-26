c1fae0aeed14ecace43b7c32e434b212ad59b958
PoRam1 <yyy3365@gmail.com>
2026-01-26 09:44:44 +0900

    [프로그래머스] 조건에 맞는 사용자와 총 거래금액 조회하기 풀이 제출
    - 문제 ID: 164668
    - 난이도: unknown
    - 제출일: 2026. 01. 26. 09:44:40
    - 작성자: 박정수

---
+++ programmers/other/164668_조건에_맞는_사용자와_총_거래금액_조회하기.py
@@ -0,0 +1,20 @@
+# 조건에 맞는 사용자와 총 거래금액 조회하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164668
+# 작성자: 박정수
+# 작성일: 2026. 01. 26. 09:44:40
+
+-- 코드를 입력하세요
+SELECT 
+    b.WRITER_ID,
+    u.NICKNAME,
+    sum(PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD as b
+left join USED_GOODS_USER as u
+    on b.WRITER_ID = u.USER_ID
+where STATUS = 'DONE'
+group by USER_ID
+having TOTAL_SALES >= '700000'
+order by TOTAL_SALES
+
+    
