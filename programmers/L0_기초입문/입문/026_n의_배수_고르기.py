288933be73d3d568b01ea822a6ff33ab1d678695
PoRam1 <yyy3365@gmail.com>
2026-01-26 09:15:26 +0900

    [프로그래머스] L0 - n의 배수 고르기 (#26)

--- programmers/L0_기초입문/입문/026_n의_배수_고르기.py
+++ programmers/L0_기초입문/입문/026_n의_배수_고르기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120905
 # 알고리즘: 기초
 # 작성자: 박정수
-# 작성일: 2026. 01. 26. 09:09:31
+# 작성일: 2026. 01. 26. 09:15:22
 
 def solution(n, numlist):
     answer = []
