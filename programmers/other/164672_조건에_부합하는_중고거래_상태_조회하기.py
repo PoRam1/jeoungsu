3089cd46b1245acc0f935e3fcd8875684c972ebb
PoRam1 <yyy3365@gmail.com>
2026-02-09 09:07:41 +0900

    [프로그래머스] 조건에 부합하는 중고거래 상태 조회하기 풀이 제출
    - 문제 ID: 164672
    - 난이도: unknown
    - 제출일: 2026. 02. 09. 09:07:39
    - 작성자: 박정수

---
+++ programmers/other/164672_조건에_부합하는_중고거래_상태_조회하기.py
@@ -0,0 +1,19 @@
+# 조건에 부합하는 중고거래 상태 조회하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164672
+# 작성자: 박정수
+# 작성일: 2026. 02. 09. 09:07:39
+
+-- 코드를 입력하세요
+SELECT 
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    case when STATUS = 'DONE' then '거래완료'
+    when STATUS = 'RESERVED' then '예약중'
+    when STATUS = 'SALE' then '판매중'
+    end AS STATUS
+from USED_GOODS_BOARD
+where DATE_format(CREATED_DATE,'%Y-%m-%d') = '2022-10-05'
+order by BOARD_ID desc
