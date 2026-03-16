ba2e896233f6d6544fd01f1d18ed7182e55c0e93
PoRam1 <yyy3365@gmail.com>
2026-03-16 09:08:28 +0900

    [프로그래머스] L0 - 짝수는 싫어요 (#40)

---
+++ programmers/L0_기초입문/입문/040_짝수는_싫어요.py
@@ -0,0 +1,13 @@
+# 짝수는 싫어요
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120813
+# 알고리즘: 기초
+# 작성자: 박정수
+# 작성일: 2026. 03. 16. 09:08:21
+
+def solution(n):
+    answer = []
+    for i in range(0, n+1):
+        if i % 2 != 0:
+            answer.append(i)
+    return answer
