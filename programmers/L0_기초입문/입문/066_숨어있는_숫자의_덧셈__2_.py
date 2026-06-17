9a4bb309fb2205ada7193aef3ff1f78ddba95574
PoRam1 <yyy3365@gmail.com>
2026-06-17 18:06:08 +0900

    [프로그래머스] L0 - 숨어있는 숫자의 덧셈 (2) (#66)

--- programmers/L0_기초입문/입문/066_숨어있는_숫자의_덧셈__2_.py
+++ programmers/L0_기초입문/입문/066_숨어있는_숫자의_덧셈__2_.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120864
 # 알고리즘: 기초
 # 작성자: 박정수
-# 작성일: 2026. 06. 17. 17:32:15
+# 작성일: 2026. 06. 17. 18:06:06
 
 def solution(my_string):
     answer = 0
