d552d2a87721b0742a67ca654cc1290129d15e20
PoRam1 <yyy3365@gmail.com>
2026-06-18 11:42:44 +0900

    [프로그래머스] L0 - 가까운 수 (#69)

---
+++ programmers/L0_기초입문/입문/069_가까운_수.py
@@ -0,0 +1,17 @@
+# 가까운 수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120890
+# 알고리즘: 기초
+# 작성자: 박정수
+# 작성일: 2026. 06. 18. 11:42:40
+
+def solution(array, n):
+    array.sort()
+
+    answer = array[0]
+
+    for num in array:
+        if abs(num - n) < abs(answer - n):
+            answer = num
+
+    return answer
